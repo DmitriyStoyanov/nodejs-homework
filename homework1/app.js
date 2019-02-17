a8fdbf391f32bd014def1dda446769bf0ee33bf9
Dmitry Stoyanov <Dmitry_Stoyanov@epam.com>
2019-02-17 16:59:39 +0300

    Homework1

---
+++ homework1/app.js
@@ -0,0 +1,8 @@
+import {User, Product} from './models'
+
+import appconfig from './config/appconfig.json';
+
+const developer = new User;
+const product = new Product;
+
+console.log(`application name: ${appconfig.name}`);
