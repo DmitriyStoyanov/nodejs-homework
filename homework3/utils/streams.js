7f77a2fead5586b6d18a5c3a084f7ce27250594c
Dmitry Stoyanov <Dmitry_Stoyanov@epam.com>
2019-03-03 21:46:00 +0300

    Added functions reverse and transform

--- homework3/utils/streams.js
+++ homework3/utils/streams.js
@@ -1,33 +1,56 @@
 #!/usr/bin/env babel-node
 import program from 'commander';
 
-function reverse (str) {
-    /* ... */
-}
-function transform (str) {
-    /* ... */
-}
-function outputFile (filePath) {
-    /* ... */
-}
-function convertFromFile (filePath) {
-    /* ... */
-}
-function convertToFile (filePath) {
-    /* ... */
-}
+const functions = {
+    reverse: function() {
+        console.log(`Could you type letters which will be reversed`);
+
+        process.stdin.on('readable', () => {
+            const letters = process.stdin.read();
+            if (letters !== null) {
+                process.stdout.write(`reversed letters: ${letters.reverse()}`);
+            }
+        });
+
+        process.stdin.on('end', () => {
+            process.stdout.write('end');
+        });
+    },
+    transform: function(str) {
+        console.log(`Could you type letters which will be upperCased`);
+
+        process.stdin.on('readable', () => {
+            const letters = process.stdin.read();
+            if (letters !== null) {
+                process.stdout.write(`UpperCased letters: ${letters.toString().toUpperCase()}`);
+            }
+        });
+
+        process.stdin.on('end', () => {
+            process.stdout.write('end');
+        });
+    },
+    outputFile: function(filePath) {
+        console.log(`outputFile ${filePath}`);
+    },
+    convertFromFile: function(filePath) {
+        console.log(`convertFromFile ${filePath}`);
+    },
+    convertToFile: function(filePath) {
+        console.log(`convertToFile ${filePath}`);
+    }
+};
 
 program
     .usage('[options]')
-    .option('-a, --action <action>', 'action to execute')
+    .option('-a, --faction <action>', 'action to execute')
     .option('-f, --file [file]', 'file for output')
     .parse(process.argv);
 
-if (typeof action === 'undefined') {
+if ( typeof program.faction === 'undefined') {
     console.error('no action given!');
     program.outputHelp();
     process.exit(1);
 }
 
-console.log('you started streams with action: %s and file: %s',
-    program.action, program.file);
+functions[program.faction](program.file);
